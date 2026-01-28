ed73efdecf3ec48be75d26ee1fcf2301971c7b9f
Gabriel Hishida <gabrielnhn@duck.com>
2026-01-28 15:19:50 +0100

    make notebook work as script

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # ScanObjectDiff3F
+
+# Install
+
+
+```bash
+conda env create -f environment.yaml                          
+conda activate diff3f                                         
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath
+ipython3
+```
+
+(Paste into ipython terminal):
+```python
+import sys
+import torch
+pyt_version_str=torch.__version__.split("+")[0].replace(".", "")
+version_str="".join([
+    f"py3{sys.version_info.minor}_cu",
+    torch.version.cuda.replace(".",""),
+    f"_pyt{pyt_version_str}"
+])
+!pip install iopath
+!pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html
+```
+
+Back to normal shell terminal
+
+```bash
+pip uninstall xformers
+pip install accelerate==0.24.0
+pip install plyfile
+pip install trimesh --no-deps --force-reinstall
+```
---
+++ og_notebook.py
@@ -0,0 +1,68 @@
+"""
+Simple correspondence workflow from ipynb file
+"""
+import sys
+sys.path.append("./diff3f/")
+# import packages
+
+
+import torch
+from diff3f2 import get_features_per_vertex
+from utils2 import convert_mesh_container_to_torch_mesh, cosine_similarity, double_plot, get_colors, generate_colors
+from time import time
+from dataloaders.mesh_container import MeshContainer
+from diffusion import init_pipe
+from dino import init_dino
+from functional_map import compute_surface_map
+
+import meshplot as mp
+mp.offline()
+
+
+device = torch.device('cuda:0')
+torch.cuda.set_device(device)
+num_views = 2
+H = 512
+W = 512
+num_images_per_prompt = 1
+tolerance = 0.004
+random_seed = 42
+use_normal_map = True
+
+def compute_features(device, pipe, dino_model, m, prompt):
+    mesh = convert_mesh_container_to_torch_mesh(m, device=device, is_tosca=False)
+    mesh_vertices = mesh.verts_list()[0]
+    features = get_features_per_vertex(
+        device=device,
+        pipe=pipe, 
+        dino_model=dino_model,
+        mesh=mesh,
+        prompt=prompt,
+        mesh_vertices=mesh_vertices,
+        num_views=num_views,
+        H=H,
+        W=W,
+        tolerance=tolerance,
+        num_images_per_prompt=num_images_per_prompt,
+        use_normal_map=use_normal_map,
+    )
+    return features.cpu()
+
+pipe = init_pipe(device)
+dino_model = init_dino(device)
+
+source_file_path = "diff3f/meshes/cow.obj"
+target_file_path = "diff3f/meshes/camel.obj"
+source_mesh = MeshContainer().load_from_file(source_file_path)
+target_mesh = MeshContainer().load_from_file(target_file_path)
+
+f_source = compute_features(device, pipe, dino_model, source_mesh, "cow")
+
+f_target = compute_features(device, pipe, dino_model, target_mesh, "camel")
+
+s = cosine_similarity(f_source.to(device),f_target.to(device))
+s = torch.argmax(s, dim=0).cpu().numpy()
+cmap_source = get_colors(source_mesh.vert); cmap_target = cmap_source[s]
+
+
+double_plot(source_mesh,target_mesh,cmap_source,cmap_target)  
